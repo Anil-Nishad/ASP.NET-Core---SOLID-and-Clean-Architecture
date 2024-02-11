63f7d377527cdf822b23fab19d6df9e8c6923a87
ANIL KUMAR <anilnishad298@gmail.com>
2024-02-11 14:42:48 +0530

    Automated-Test-(Integrated-Unit)

--- HR.LeaveManagement.Clean/HR.LeaveManagement.Application/obj/Debug/net8.0/HR.LeaveManagement.Application.AssemblyInfo.cs
+++ HR.LeaveManagement.Clean/HR.LeaveManagement.Application/obj/Debug/net8.0/HR.LeaveManagement.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HR.LeaveManagement.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4598bfcc1f2f33b84683b3161c0761c738f4c99")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73da54447d6dfa243ffacab97a9433cb7edae327")]
 [assembly: System.Reflection.AssemblyProductAttribute("HR.LeaveManagement.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HR.LeaveManagement.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ HR.LeaveManagement.Clean/HR.LeaveManagementApplication.UnitTests/Mocks/MockLeaveTypeRepository.cs
@@ -0,0 +1,52 @@
+﻿using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Domain;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagementApplication.UnitTests.Mocks
+{
+    public class MockLeaveTypeRepository
+    {
+        public static Mock<ILeaveTypeRepository> GetMockLeaveTypeRepository()
+        {
+            var leaveTypes = new List<LeaveType>
+            {
+                new LeaveType
+                {
+                    Id = 1,
+                    DefaultDays = 10,
+                    Name = "Test Vacation"
+                },
+                new LeaveType
+                {
+                    Id = 2,
+                    DefaultDays = 15,
+                    Name = "Test Sick"
+                },
+                new LeaveType
+                {
+                    Id = 3,
+                    DefaultDays = 15,
+                    Name = "Test Maternity"
+                }
+            };
+
+            var mockRepo = new Mock<ILeaveTypeRepository>();
+
+            mockRepo.Setup(r => r.GetAsync()).ReturnsAsync(leaveTypes);
+
+            mockRepo.Setup(r => r.CreateAsync(It.IsAny<LeaveType>()))
+                .Returns((LeaveType leaveType) =>
+                {
+                    leaveTypes.Add(leaveType);
+                    return Task.CompletedTask;
+                });
+
+            return mockRepo;
+        }
+    }
+}
